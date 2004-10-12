2f1744366370ef31ab1dec16f667ebaea1bb82d1
christi willison <willison@pbworld.com>
2004-10-12 21:33:43 +0000

    added some other trip modes
    git-svn-id: http://216.243.97.229:9090/svn/projects/trunk/tlumip@9 ee1c2863-5742-f242-9f82-d2f51c100d09

--- src/java/com/pb/despair/model/ModeType.java
+++ src/java/com/pb/despair/model/ModeType.java
@@ -14,5 +14,10 @@ public final class ModeType {
     public final static int DRIVEALONE              = 1;
     public final static int SHAREDRIDE2             = 2;
     public final static int SHAREDRIDE3PLUS         = 3;
-     
+    public final static int WALKTRIP                = 4;
+    public final static int BIKETRIP                = 5;
+    public static final int WALKTRANSITTRIP         = 6;
+    public static final int DRIVETRANSITTRIP        = 7;
+
+    
 }
