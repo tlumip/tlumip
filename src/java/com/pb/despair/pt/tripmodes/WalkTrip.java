e0e67f50e0b7cf95d6589f0f18718e1b56639e12
christi willison <willison@pbworld.com>
2004-10-12 21:41:34 +0000

    added new trip mode
    git-svn-id: http://216.243.97.229:9090/svn/projects/trunk/tlumip@17 ee1c2863-5742-f242-9f82-d2f51c100d09

---
+++ src/java/com/pb/despair/pt/tripmodes/WalkTrip.java
@@ -0,0 +1,26 @@
+package com.pb.despair.pt.tripmodes;
+import com.pb.despair.model.Mode;
+import com.pb.despair.model.ModeType;
+import com.pb.despair.model.TravelTimeAndCost;
+import com.pb.despair.pt.Activity;
+import com.pb.despair.pt.PersonTripModeAttributes;
+import com.pb.despair.pt.TripModeParameters;
+import com.pb.despair.pt.ZoneAttributes;
+
+import java.util.logging.Logger;
+/** 
+ * Driver alone mode
+ * 
+ * @author Joel Freedman
+ * @version 1.0 12/01/2003
+ * 
+ */
+public class WalkTrip extends Mode {
+    protected static Logger logger = Logger.getLogger("com.pb.despair.pt.default");
+     public WalkTrip(){
+          alternativeName=new String("WalkTrip");
+          type=ModeType.WALKTRIP;
+     }
+
+}
+
