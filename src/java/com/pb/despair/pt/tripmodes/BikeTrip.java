f208ff77c2f5be7c00ddf05cc0bec621dfcc73aa
christi willison <willison@pbworld.com>
2004-10-12 21:40:21 +0000

    added new trip mode
    git-svn-id: http://216.243.97.229:9090/svn/projects/trunk/tlumip@13 ee1c2863-5742-f242-9f82-d2f51c100d09

--- src/java/com/pb/despair/pt/tripmodes/BikeTrip.java
+++ src/java/com/pb/despair/pt/tripmodes/BikeTrip.java
@@ -17,59 +17,9 @@ import java.util.logging.Logger;
  */
 public class BikeTrip extends Mode {
     protected static Logger logger = Logger.getLogger("com.pb.despair.pt.default");
-     
-//     public boolean isAvailable=true;
-//     public boolean hasUtility=false;
-     
- //    double utility=0;
-
-     public BikeTrip(){
-         isAvailable = true;
-                hasUtility = false;
-                utility = 0.0D;
-          alternativeName=new String("DriveAlone");
-          type=ModeType.DRIVEALONE;
+    public BikeTrip(){
+          alternativeName=new String("BikeTrip");
+          type=ModeType.BIKETRIP;
      }
-     
-    /** Calculates utility of driving alone
-     * 
-     * @param inbound - In-bound TravelTimeAndCost
-     * @param outbound - Outbound TravelTimeAndCost
-     * @param z - ZoneAttributes (Currently only parking cost)
-     * @param c - TourModeParameters
-     * @param p - PersonTourModeAttributes
-     */
-     public void calcUtility(TravelTimeAndCost tc, ZoneAttributes z,TripModeParameters c, PersonTripModeAttributes p,Mode tourMode,
-          Activity thisActivity){
-               
-               if(p.age<16) isAvailable=false;
-               if(p.autos==0) isAvailable=false;
-               if(tourMode.type!=ModeType.AUTODRIVER) isAvailable=false;
-               
-               if(isAvailable){
-                    time=(tc.driveAloneTime);
-                    utility=(
-                      c.ivt*(tc.driveAloneTime )
-                 + c.opclow*(tc.driveAloneCost*p.inclow)
-                 + c.opcmed*(tc.driveAloneCost*p.incmed)
-                 + c.opchi* (tc.driveAloneCost*p.inchi)
-                 + c.pkglow*((z.parkingCost*(thisActivity.duration/60))*p.inclow)
-                 + c.pkgmed*((z.parkingCost*(thisActivity.duration/60))*p.incmed)
-                 + c.pkghi*((z.parkingCost*(thisActivity.duration/60))*p.inchi)
-                    );
-               hasUtility=true;
-               };
-     };
-    /** Get drive alone utility */
-     public double getUtility(){
-          if(!hasUtility){
-               logger.severe("Error: Utility not calculated for "+alternativeName+"\n");
-               
-                System.exit(1);
-          };
-          return utility;
-     };
-     
-
 }
 
