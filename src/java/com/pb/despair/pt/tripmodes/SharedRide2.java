56811e95016892934f2b6f27c00155326774c735
christi willison <willison@pbworld.com>
2004-10-12 21:40:47 +0000

    added new trip mode
    git-svn-id: http://216.243.97.229:9090/svn/projects/trunk/tlumip@15 ee1c2863-5742-f242-9f82-d2f51c100d09

--- src/java/com/pb/despair/pt/tripmodes/SharedRide2.java
+++ src/java/com/pb/despair/pt/tripmodes/SharedRide2.java
@@ -40,9 +40,14 @@ public class SharedRide2 extends Mode {
      
      public void calcUtility(TravelTimeAndCost tc, ZoneAttributes z,TripModeParameters c, PersonTripModeAttributes p, Mode tourMode,
           Activity thisActivity){
-               
+
+              hasUtility = false;
+              utility=-999;
+              isAvailable = true;
+
           if(tc.sharedRide2Time==0) isAvailable=false;
-          if(tourMode.type!=ModeType.AUTODRIVER && tourMode.type!=ModeType.AUTOPASSENGER)
+          if(tourMode.type!=ModeType.AUTODRIVER && tourMode.type!=ModeType.AUTOPASSENGER
+             && tourMode.type!=ModeType.TRANSITPASSENGER && tourMode.type!=ModeType.PASSENGERTRANSIT)
                isAvailable=false;
                
           int autoDriver=0;
