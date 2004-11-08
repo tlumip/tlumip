52453469521c0977c73122b3159c146d995ddb8a
christi willison <willison@pbworld.com>
2004-11-08 22:41:22 +0000

    moved the PIModel object creation out of the onStart method.
    git-svn-id: http://216.243.97.229:9090/svn/projects/trunk/tlumip@108 ee1c2863-5742-f242-9f82-d2f51c100d09

--- src/java/com/pb/despair/pi/daf/CUWorkTask.java
+++ src/java/com/pb/despair/pi/daf/CUWorkTask.java
@@ -26,39 +26,45 @@ public class CUWorkTask extends MessageProcessingTask {
     boolean debug = false;
     private ResourceBundle pidafRb;
     String scenarioName = "pleaseWork";
+    private boolean firstMessage = true;
 
     public void onStart() {
 
-        logger.info("*******************************************************************************************");
-        logger.info( "***" + getName() + " is starting...");
-
-        pidafRb = ResourceUtil.getResourceBundle("pidaf_"+scenarioName);
-        //We need to read in the Run Parameters (timeInterval and pathToResourceBundle) from the RunParams.txt file
-        //that was written by the Application Orchestrator
-        BufferedReader reader = null;
-        int timeInterval = -1;
-        String pathToRb = null;
-        String scenarioName = null;
-        try {
-            logger.info("Reading RunParams.txt file");
-            reader = new BufferedReader(new FileReader(new File((String)ResourceUtil.getProperty(pidafRb,"run.param.file"))));
-            scenarioName = reader.readLine();
-            logger.info("\tScenario Name: " + scenarioName);
-            timeInterval = Integer.parseInt(reader.readLine());
-            logger.info("\tTime Interval: " + timeInterval);
-            pathToRb = reader.readLine();
-            logger.info("\tResourceBundle Path: " + pathToRb);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        ResourceBundle rb = ResourceUtil.getPropertyBundle(new File(pathToRb));
-        logger.info("  *" + getName() + " is creating a PIModel Object");
-        pi = new PIModel(rb);
-        logger.info("*******************************************************************************************");
+        logger.info("***************************" + getName() + " begin onStart() *****************************************");
+//        logger.info( "***" + getName() + " is starting...");
+//        //Getting the properties file
+//        logger.info(" *** + " + getName() + " is getting the pidaf resource bundle");
+//        pidafRb = ResourceUtil.getResourceBundle("pidaf_"+scenarioName);
+//        //We need to read in the Run Parameters (timeInterval and pathToResourceBundle) from the RunParams.txt file
+//        //that was written by the Application Orchestrator
+//        BufferedReader reader = null;
+//        int timeInterval = -1;
+//        String pathToRb = null;
+//        String scenarioName = null;
+//        try {
+//            logger.info("Reading RunParams.txt file");
+//            reader = new BufferedReader(new FileReader(new File(ResourceUtil.getProperty(pidafRb,"run.param.file"))));
+//            scenarioName = reader.readLine();
+//            logger.info("\tScenario Name: " + scenarioName);
+//            timeInterval = Integer.parseInt(reader.readLine());
+//            logger.info("\tTime Interval: " + timeInterval);
+//            pathToRb = reader.readLine();
+//            logger.info("\tResourceBundle Path: " + pathToRb);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        ResourceBundle rb = ResourceUtil.getPropertyBundle(new File(pathToRb));
+//        logger.info("  *" + getName() + " is creating a PIModel Object");
+//        pi = new PIModel(rb);
+        logger.info("***************************" + getName() + " end onStart()****************************************");
     }
 
     public void onMessage(Message msg) {
         if(debug) logger.info( getName() + " received " + msg.getStringValue("Name") + " from" + msg.getSender() );
+        if(firstMessage){
+            pi = new PIModel(SetupWorkTask.rb);
+            firstMessage = false;
+        }
         String name = msg.getStringValue("Name");
         double[] prices = (double[])msg.getValue("Price");
 
