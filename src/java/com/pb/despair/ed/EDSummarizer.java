580e5222ac53b23561c2bcd90624119fefce5a55
christi willison <willison@pbworld.com>
2004-12-15 22:46:39 +0000

    upgraded the calibration output routines
    git-svn-id: http://216.243.97.229:9090/svn/projects/trunk/tlumip@216 ee1c2863-5742-f242-9f82-d2f51c100d09

--- src/java/com/pb/despair/ed/EDSummarizer.java
+++ src/java/com/pb/despair/ed/EDSummarizer.java
@@ -3,11 +3,14 @@ package com.pb.despair.ed;
 import com.pb.common.datafile.TableDataSet;
 import com.pb.common.datafile.CSVFileReader;
 import com.pb.common.datafile.CSVFileWriter;
+import com.pb.common.util.ResourceUtil;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.logging.Logger;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
+import java.util.Iterator;
 
 /**
  * Author: willison
@@ -19,125 +22,118 @@ public class EDSummarizer {
     static Logger logger = Logger.getLogger("com.pb.despair.ed");
     static String[] header = null;
     static float[][] outputValues = null;
-    
-    public static void writeActivityCalibrationOutputFile(File prevYearCalibrationFile, File dataFile,
-                                                             int year, String outputFilePath){
-        createCalibrationOutputTable(prevYearCalibrationFile, dataFile, year, "Activity");
-        writeCalibrationOutputFile(outputFilePath, "Activity");
+
+
+    public static TableDataSet summarize(String output, int timeInterval, int baseYear, ResourceBundle appRb, ArrayList colHeaders){
+
+        //Read the model_data.csv file that is ED's output file so that we can process the data into a calibration file
+        File resultsFile = new File(ResourceUtil.getProperty(appRb, "defaultDataLocation"));
+        TableDataSet model_data = readModelDataFile(resultsFile);
+
+
+        TableDataSet dataSet = null;
+
+        if(output.equalsIgnoreCase("oregon.activity")){
+            dataSet = summarizeOregonActivity(timeInterval, baseYear, model_data, colHeaders);
+        }else if(output.equalsIgnoreCase("oregon.employment"))
+            dataSet = summarizeOregonEmployment(timeInterval, baseYear, model_data);
+        else if(output.equalsIgnoreCase("odot.employment"))
+            dataSet = summarizeODOTEmployment(timeInterval, baseYear, model_data, colHeaders);
+        else
+            logger.warning("No method has been developed to summarize " + output + ".  ED can" +
+                    "currently produce the oregon activity, oregon employment and odot employement summaries -" +
+                    "check calibration.properties");
+        return dataSet;
     }
 
-    public static void writeEmploymentCalibrationOutputFile(File prevYearCalibrationFile, File dataFile,
-                                                             int year, String outputFilePath){
-        createCalibrationOutputTable(prevYearCalibrationFile, dataFile, year, "Employment");
-        writeCalibrationOutputFile(outputFilePath, "Employment");
+    private static TableDataSet summarizeOregonActivity(int timeInterval, int baseYear, TableDataSet model_data, ArrayList actHeaders){
+
+        actHeaders.add(0,"year");
+        float[][] values = new float[timeInterval][actHeaders.size()];
+
+        int startIndex = findRowInTable(model_data, baseYear+1);
+
+        for(int r=0; r< values.length; r++){
+            for(int c=0; c< values[0].length; c++){
+                values[r][c] =  model_data.getValueAt(startIndex+r, (String)actHeaders.get(c));
+            }
+        }
+
+        TableDataSet data = TableDataSet.create(values,actHeaders);
+        return data;
     }
 
-    public static void writeConstructionCalibrationOutputFile(File prevYearCalibrationFile, File dataFile,
-                                                             int year, String outputFilePath){
-        createCalibrationOutputTable(prevYearCalibrationFile, dataFile, year, "Construction"); 
-        writeCalibrationOutputFile(outputFilePath, "Construction");
-    }
+    private static TableDataSet summarizeOregonEmployment(int timeInterval, int baseYear, TableDataSet model_data){
 
-    public static void writePopulationCalibrationOutputFile(File prevYearCalibrationFile, File dataFile,
-                                                             int year, String outputFilePath){
-        createCalibrationOutputTable(prevYearCalibrationFile, dataFile, year, "Population");
-        writeCalibrationOutputFile(outputFilePath, "Population");
-    }
-
-
-
-    private static void createCalibrationOutputTable(File prevYearCalibrationOutputFile,
-                                                    File dataFile, int year, String type) {
-
-        //Read the model_data.csv file that is ED's output file so that we can copy the data into our new output file
-        TableDataSet model_data = readModelDataFile(dataFile);
+        //First get the headers that you need out of the model_data file.  The
+        //headers are unique in that they all start with OEMP so there is no need to list
+        //them in the calibration.properties file the way we do with the activity headers.
         String[] model_dataHeaders = model_data.getColumnLabels();
-        //find the correct row to extract the data from.
-        int rowIndex = findRowInTable(model_data, year);
-
-        float[] currentValues = null;
-        if(prevYearCalibrationOutputFile.exists()){  //read in existing calibration output and add current output to it.
-            try {
-                CSVFileReader reader = new CSVFileReader();
-                TableDataSet prevYearOutputDataTable = reader.readFile(prevYearCalibrationOutputFile);
-                header = prevYearOutputDataTable.getColumnLabels();
-                float[][] prevValues = prevYearOutputDataTable.getValues();
-
-                //Get the current values using the headers from the prev year's output
-                currentValues = getCurrentValues( model_data, rowIndex, header );
-
-                //Now create a new set of outputValues which will be turned into a TableDataSet.
-                outputValues = new float[prevValues.length+1][header.length]; //1 extra row, same number of columns
-                for(int r=0; r< outputValues.length; r++){
-                    for(int c=0; c< outputValues[0].length; c++){
-                        if(r != outputValues.length-1){   //for all rows but the last row, copy the prevYearData.
-                            outputValues[r][c] = prevValues[r][c];
-                        }else{                            //on the last row, copy the current data.
-                            outputValues[r][c] = currentValues[c];
-                        }
-                    }
-                }
-
-             } catch (IOException e) {
-                e.printStackTrace();
+        ArrayList empHeader = new ArrayList();
+        empHeader.add(0,"year");
+        int count = 1;
+        for(int i=1; i< model_dataHeaders.length; i++){
+            if(model_dataHeaders[i].startsWith("OEMP")){       // all headers starting with "OEMP" need to be added to
+                empHeader.add(count, model_dataHeaders[i]);    // the employment calibration file
+                count++;
             }
-        } else { // no previous file so we need to get the headers we want out of the model_data file
-            ArrayList file1Header = new ArrayList();
-            file1Header.add(0,"year");
-
-            int file1Count = 1;
-            for(int i=1; i< model_dataHeaders.length; i++){
-                if(type.equals("Activity")){   //the headers will depend on the type of calibration file we are creating.
-                    if(model_dataHeaders[i].startsWith("PROD")){       // all headers starting with "PROD" need to be added to
-                        file1Header.add(file1Count, model_dataHeaders[i]);    // the production calibration file
-                        file1Count++;
-                    }
-                } else if(type.equals("Employment")){
-                    if(model_dataHeaders[i].startsWith("OEMP")){       // all headers starting with "OEMP" need to be added to
-                        file1Header.add(file1Count, model_dataHeaders[i]);    // the employment calibration file
-                        file1Count++;
-                    }
-                } else if(type.equals("Construction")){              //all headers ending in "RSV" need to be added
-                    if(model_dataHeaders[i].endsWith("RSV")){           //to the construction calibration file
-                        file1Header.add(file1Count, model_dataHeaders[i]);
-                        file1Count++;
-                    }
-                } else if(type.equals("Population")){              //one header called OPOP needs to be added
-                    if(model_dataHeaders[i].equalsIgnoreCase("OPOP")){     //to the population calibration file
-                        file1Header.add(file1Count, model_dataHeaders[i]);
-                        file1Count++;
-                    }
-                }
-            }
-            //Set header
-            header = new String[file1Header.size()];
-            for(int i=0; i< file1Header.size(); i++){
-                header[i] = (String)file1Header.get(i);
-            }
-            //Get current values
-            currentValues = getCurrentValues(model_data,rowIndex,header);
-            //Set outputValues
-            outputValues = new float[1][header.length];
-            for(int i=0; i< outputValues[0].length; i++){
-                outputValues[0][i] = currentValues[i];
-            }
-            
-        }
-    }
-
-    private static void writeCalibrationOutputFile(String outputFilePath, String type){
-        logger.info("Writing " + outputFilePath);
-        TableDataSet outputTable = TableDataSet.create(outputValues, header);
-        outputTable.setName(type + " Calibration Output");
-        CSVFileWriter writer = new CSVFileWriter();
-        try {
-            writer.writeFile(outputTable, new File(outputFilePath));
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
 
+        //figure out which row we need to extract data from
+        int startIndex = findRowInTable(model_data, baseYear+1);
+
+        //create an array of values
+        float[][] values = new float[timeInterval][empHeader.size()];
+
+        //fill up the values array
+        for(int r=0; r< values.length; r++){
+            for(int c=0; c< values[0].length; c++){
+                values[r][c] =  model_data.getValueAt(startIndex+r, (String)empHeader.get(c));
+            }
+        }
+
+        //pass back the values.
+        TableDataSet data = TableDataSet.create(values,empHeader);
+        return data;
+
     }
+
+    private static TableDataSet summarizeODOTEmployment(int timeInterval, int baseYear, TableDataSet model_data, ArrayList odotHeader){
+        //This is similar to the oregon employment except that certain columns will be summed together
+        //for example ACCOMMODATION + PERSONAL SERVICES + HOMEBASED SERVICES will be one category called
+        //OEMPACCPERSHOME instead of the three separate categories.
+        //We start out by adding year to our list of column headers
+        odotHeader.add(0,"year");
+
+        //define our array of values
+        float[][] values = new float[timeInterval][odotHeader.size()];
+
+        //figure out where to start
+        int startIndex = findRowInTable(model_data, baseYear+1);
+
+        //fill up the values array
+        for(int r=0; r< values.length; r++){
+            for(int c=0; c< values[0].length; c++){
+                if(c==1) values[r][c] = model_data.getValueAt(startIndex+r, "OEMPACC") +
+                        model_data.getValueAt(startIndex+r, "OEMPPERS") + model_data.getValueAt(startIndex+r, "OEMPHOME");
+                else if (c==10) values[r][c] = model_data.getValueAt(startIndex+r, "OEMPHIED") +
+                        model_data.getValueAt(startIndex+r, "OEMPLOED");
+                else if (c==12) values[r][c] = model_data.getValueAt(startIndex+r, "OEMPPAP") +
+                        model_data.getValueAt(startIndex+r, "OEMPOND");
+                else if(c==14) values[r][c] = model_data.getValueAt(startIndex+r, "OEMPTRAN") +
+                        model_data.getValueAt(startIndex+r, "OEMPCOM");
+                else values[r][c] =  model_data.getValueAt(startIndex+r, (String)odotHeader.get(c));
+            }
+        }
+
+        //pass back the values.
+        TableDataSet data = TableDataSet.create(values,odotHeader);
+        return data;
+
+
+    }
+
+
 
     private static TableDataSet readModelDataFile (File dataFile){
         CSVFileReader reader = new CSVFileReader();
@@ -165,17 +161,6 @@ public class EDSummarizer {
             logger.info("Summarizing data from row " + rowIndex + " corresponding to year " + (int)table.getValueAt(rowIndex,"year"));
         }
         return rowIndex;
-    }
-
-    private static float[] getCurrentValues(TableDataSet table, int row, String[] columnNames){
-        //get the data corresponding to the correct column (indicated by header index) and
-        //put it into a temp. array.  These data will be added to the prevValues and then written
-        //out.
-        float[] currentValues = new float[header.length];
-        for(int i=0; i< currentValues.length; i++){
-            currentValues[i] = table.getValueAt(row,header[i]);
-        }
-        return currentValues;
     }
 
 
