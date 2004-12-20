0ac869e43c44f616385d512c2c0933cd3093370d
jim hicks <hicksji@pbworld.com>
2004-12-20 23:25:29 +0000

    industry category labels and order taken from input file instead of defaults in EdIndustry
    git-svn-id: http://216.243.97.229:9090/svn/projects/trunk/tlumip@222 ee1c2863-5742-f242-9f82-d2f51c100d09

--- src/java/com/pb/despair/spg/EdIndustry.java
+++ src/java/com/pb/despair/spg/EdIndustry.java
@@ -11,101 +11,11 @@ public class EdIndustry {
 
     int[] pumsToEd = new int[1000];
     
-    String[] edIndustryLabels = {
-		"ACCOMMODATIONS",
-		"AGRICULTURE AND MINING",
-		"COMMUNICATIONS AND UTILITIES",
-		"CONSTRUCTION",
-		"ELECTRONICS AND INSTRUMENTS-Light Industry",
-		"FIRE BUSINESS AND PROFESSIONAL SERVICES",
-		"FOOD PRODUCTS-Heavy Industry",
-		"FOOD PRODUCTS-Light Industry",
-		"FORESTRY AND LOGGING",
-		"GOVERNMENT ADMINISTRATION",
-		"HEALTH SERVICES-Hospital",
-		"HEALTH SERVICES-Institutional",
-		"HEALTH SERVICES-Office",
-		"HIGHER EDUCATION",
-		"HOMEBASED SERVICES",
-		"LOWER EDUCATION",
-		"LUMBER AND WOOD PRODUCTS-Heavy Industry",
-		"OTHER DURABLES-Heavy Industry",
-		"OTHER DURABLES-Light Industry",
-		"OTHER NON-DURABLES-Heavy Industry",
-		"OTHER NON-DURABLES-Light Industry",
-		"PERSONAL AND OTHER SERVICES AND AMUSEMENTS",
-		"PULP AND PAPER-Heavy Industry",
-		"RETAIL TRADE",
-		"TRANSPORT",
-		"WHOLESALE TRADE",
-		"UNEMPLOYED"
-    };
-    
-/*
-	int[] edIndustryEmployment = {
-		64,
-		188, 
-		45,
-		239,
-		71,
-		546,
-		30,
-		15,
-		17, 
-		248,
-		83,
-		34,
-		109,
-		96,
-		21,
-		149,
-		70,
-		82,
-		42,
-		19,
-		33,
-		326,
-		19,
-		574,
-		105,
-		161
-	};
-*/
-    
-	float[] edIndustryEmployment = {
-		64997,
-		188146, 
-		45304,
-		239022,
-		71484,
-		546557,
-		30776,
-		15412,
-		17838, 
-		248056,
-		83617,
-		34723,
-		109699,
-		96966,
-		21337,
-		149835,
-		70874,
-		82635,
-		42884,
-		19220,
-		33964,
-		326965,
-		19968,
-		574600,
-		105767,
-		161896
-	};
+    float[] edIndustryEmployment;
+    String[] edIndustryLabels;
     
     
     public EdIndustry () {
-
-		// establish the correspondence table between pums codes and ED industry categories
-		pumsIndustryToEdIndustry();
 
     }
 
@@ -171,6 +81,9 @@ public class EdIndustry {
 		edIndustryLabels[tempEdIndustryLabels.length] = "UNEMPLOYED";
 		edIndustryEmployment[tempEdIndustryLabels.length] = 0.0f;
 		
+		// establish the correspondence table between pums codes and ED industry categories
+		pumsIndustryToEdIndustry();
+
 		return edIndustryEmployment;
 		
 	}
@@ -178,156 +91,186 @@ public class EdIndustry {
 	
 	// define the pums/ed industry index correspondence table
     private void pumsIndustryToEdIndustry () {
+    	
+    	int industryIndex = 0;
+    	
         
         //ACCOMMODATIONS
+    	industryIndex = getIndustryIndex("ACCOMMODATIONS");
 		for (int i=762; i <= 770; i++)
-			pumsToEd[i] = 0;
+			pumsToEd[i] = industryIndex;
         
         //AGRICULTURE AND MINING
+    	industryIndex = getIndustryIndex("AGRICULTURE AND MINING");
 		for (int i=1; i <= 30; i++)
-			pumsToEd[i] = 1;
+			pumsToEd[i] = industryIndex;
 		for (int i=32; i <= 59; i++)
-			pumsToEd[i] = 1;
+			pumsToEd[i] = industryIndex;
 		
 		//COMMUNICATIONS AND UTILITIES
+    	industryIndex = getIndustryIndex("COMMUNICATIONS AND UTILITIES");
 		for (int i=440; i <= 499; i++)
-			pumsToEd[i] = 2;
+			pumsToEd[i] = industryIndex;
 
 		//CONSTRUCTION
+    	industryIndex = getIndustryIndex("CONSTRUCTION");
 		for (int i=60; i <= 99; i++)
-			pumsToEd[i] = 3;
+			pumsToEd[i] = industryIndex;
 
 		//ELECTRONICS AND INSTRUMENTS-Light Industry
+    	industryIndex = getIndustryIndex("ELECTRONICS AND INSTRUMENTS-Light Industry");
 		for (int i=322; i <= 330; i++)
-			pumsToEd[i] = 4;
+			pumsToEd[i] = industryIndex;
 		for (int i=371; i <= 380; i++)
-			pumsToEd[i] = 4;
+			pumsToEd[i] = industryIndex;
 
 		//FIRE BUSINESS AND PROFESSIONAL SERVICES
+    	industryIndex = getIndustryIndex("FIRE BUSINESS AND PROFESSIONAL SERVICES");
 		for (int i=700; i <= 741; i++)
-		    pumsToEd[i] = 5;
+		    pumsToEd[i] = industryIndex;
 		for (int i=882; i <= 899; i++)
-			pumsToEd[i] = 5;
-		pumsToEd[841] = 5;
+			pumsToEd[i] = industryIndex;
+		pumsToEd[841] = industryIndex;
 
 		//FOOD PRODUCTS-Heavy Industry
+    	industryIndex = getIndustryIndex("FOOD PRODUCTS-Heavy Industry");
 		for (int i=100; i <= 110; i++)
-			pumsToEd[i] = 6;
+			pumsToEd[i] = industryIndex;
 
 		//FOOD PRODUCTS-Light Industry
+    	industryIndex = getIndustryIndex("FOOD PRODUCTS-Light Industry");
 		for (int i=111; i <= 129; i++)
-			pumsToEd[i] = 7;
+			pumsToEd[i] = industryIndex;
 		
 		//FORESTRY AND LOGGING
-		pumsToEd[31] = 8;
-		pumsToEd[230] = 8;
+    	industryIndex = getIndustryIndex("FORESTRY AND LOGGING");
+		pumsToEd[31] = industryIndex;
+		pumsToEd[230] = industryIndex;
 
 		//GOVERNMENT ADMINISTRATION
+    	industryIndex = getIndustryIndex("GOVERNMENT ADMINISTRATION");
 		for (int i=900; i <= 999; i++)
-			pumsToEd[i] = 9;
+			pumsToEd[i] = industryIndex;
 		
 		//HEALTH SERVICES-Hospital
+    	industryIndex = getIndustryIndex("HEALTH SERVICES-Hospital");
 		for (int i=833; i <= 839; i++)
-			pumsToEd[i] = 10;
-		pumsToEd[831] = 10;
+			pumsToEd[i] = industryIndex;
+		pumsToEd[831] = industryIndex;
 		
 		//HEALTH SERVICES-Institutional
-		pumsToEd[832] = 11;
+    	industryIndex = getIndustryIndex("HEALTH SERVICES-Institutional");
+		pumsToEd[832] = industryIndex;
 		
 		//HEALTH SERVICES-Office
+    	industryIndex = getIndustryIndex("HEALTH SERVICES-Office");
 		for (int i=812; i <= 830; i++)
-			pumsToEd[i] = 12;
-		pumsToEd[840] = 12;
+			pumsToEd[i] = industryIndex;
+		pumsToEd[840] = industryIndex;
 		
 		//HIGHER EDUCATION
-		pumsToEd[850] = 13;
-		pumsToEd[851] = 13;
+    	industryIndex = getIndustryIndex("HIGHER EDUCATION");
+		pumsToEd[850] = industryIndex;
+		pumsToEd[851] = industryIndex;
 		
 		//HOMEBASED SERVICES
-		pumsToEd[761] = 14;
+    	industryIndex = getIndustryIndex("HOMEBASED SERVICES");
+		pumsToEd[761] = industryIndex;
 		
 		//LOWER EDUCATION
+    	industryIndex = getIndustryIndex("LOWER EDUCATION");
 		for (int i=842; i <= 849; i++)
-			pumsToEd[i] = 15;
+			pumsToEd[i] = industryIndex;
 		
 		//LUMBER AND WOOD PRODUCTS-Heavy Industry
+    	industryIndex = getIndustryIndex("LUMBER AND WOOD PRODUCTS-Heavy Industry");
 		for (int i=231; i <= 241; i++)
-			pumsToEd[i] = 16;
+			pumsToEd[i] = industryIndex;
 		
 		//OTHER DURABLES-Heavy Industry
+    	industryIndex = getIndustryIndex("OTHER DURABLES-Heavy Industry");
 		for (int i=251; i <= 260; i++)
-			pumsToEd[i] = 17;
+			pumsToEd[i] = industryIndex;
 		for (int i=262; i <= 280; i++)
-			pumsToEd[i] = 17;
+			pumsToEd[i] = industryIndex;
 		for (int i=282; i <= 300; i++)
-			pumsToEd[i] = 17;
+			pumsToEd[i] = industryIndex;
 		for (int i=302; i <= 320; i++)
-			pumsToEd[i] = 17;
+			pumsToEd[i] = industryIndex;
 		for (int i=331; i <= 340; i++)
-			pumsToEd[i] = 17;
+			pumsToEd[i] = industryIndex;
 		for (int i=342; i <= 349; i++)
-			pumsToEd[i] = 17;
+			pumsToEd[i] = industryIndex;
 		for (int i=351; i <= 369; i++)
-			pumsToEd[i] = 17;
+			pumsToEd[i] = industryIndex;
 		
 		//OTHER DURABLES-Light Industry
+    	industryIndex = getIndustryIndex("OTHER DURABLES-Light Industry");
 		for (int i=242; i <= 250; i++)
-			pumsToEd[i] = 18;
+			pumsToEd[i] = industryIndex;
 		for (int i=381; i <= 399; i++)
-			pumsToEd[i] = 18;
-		pumsToEd[261] = 18;
-		pumsToEd[281] = 18;
-		pumsToEd[301] = 18;
-		pumsToEd[321] = 18;
-		pumsToEd[341] = 18;
-		pumsToEd[350] = 18;
-		pumsToEd[370] = 18;
+			pumsToEd[i] = industryIndex;
+		pumsToEd[261] = industryIndex;
+		pumsToEd[281] = industryIndex;
+		pumsToEd[301] = industryIndex;
+		pumsToEd[321] = industryIndex;
+		pumsToEd[341] = industryIndex;
+		pumsToEd[350] = industryIndex;
+		pumsToEd[370] = industryIndex;
 		
 		//OTHER NON-DURABLES-Heavy Industry
+    	industryIndex = getIndustryIndex("OTHER NON-DURABLES-Heavy Industry");
 		for (int i=141; i <= 150; i++)
-			pumsToEd[i] = 19;
+			pumsToEd[i] = industryIndex;
 		for (int i=180; i <= 220; i++)
-			pumsToEd[i] = 19;
-		pumsToEd[132] = 19;
+			pumsToEd[i] = industryIndex;
+		pumsToEd[132] = industryIndex;
 		
 		//OTHER NON-DURABLES-Light Industry
+    	industryIndex = getIndustryIndex("OTHER NON-DURABLES-Light Industry");
 		for (int i=133; i <= 140; i++)
-			pumsToEd[i] = 20;
+			pumsToEd[i] = industryIndex;
 		for (int i=151; i <= 159; i++)
-			pumsToEd[i] = 20;
+			pumsToEd[i] = industryIndex;
 		for (int i=171; i <= 179; i++)
-			pumsToEd[i] = 20;
+			pumsToEd[i] = industryIndex;
 		for (int i=221; i <= 229; i++)
-			pumsToEd[i] = 20;
-		pumsToEd[130] = 20;
-		pumsToEd[131] = 20;
+			pumsToEd[i] = industryIndex;
+		pumsToEd[130] = industryIndex;
+		pumsToEd[131] = industryIndex;
 		
 		//PERSONAL AND OTHER SERVICES AND AMUSEMENTS
+    	industryIndex = getIndustryIndex("PERSONAL AND OTHER SERVICES AND AMUSEMENTS");
 		for (int i=742; i <= 760; i++)
-			pumsToEd[i] = 21;
+			pumsToEd[i] = industryIndex;
 		for (int i=771; i <= 811; i++)
-			pumsToEd[i] = 21;
+			pumsToEd[i] = industryIndex;
 		for (int i=852; i <= 881; i++)
-			pumsToEd[i] = 21;
+			pumsToEd[i] = industryIndex;
 
 		//PULP AND PAPER-Heavy Industry
+    	industryIndex = getIndustryIndex("PULP AND PAPER-Heavy Industry");
 		for (int i=160; i <= 170; i++)
-			pumsToEd[i] = 22;
+			pumsToEd[i] = industryIndex;
 
 		//RETAIL TRADE
+    	industryIndex = getIndustryIndex("RETAIL TRADE");
 		for (int i=580; i <= 699; i++)
-			pumsToEd[i] = 23;
+			pumsToEd[i] = industryIndex;
 
 		//TRANSPORT
+    	industryIndex = getIndustryIndex("TRANSPORT");
 		for (int i=400; i <= 439; i++)
-			pumsToEd[i] = 24;
+			pumsToEd[i] = industryIndex;
 
 		//WHOLESALE TRADE
+    	industryIndex = getIndustryIndex("WHOLESALE TRADE");
 		for (int i=500; i <= 579; i++)
-			pumsToEd[i] = 25;
+			pumsToEd[i] = industryIndex;
 
 		//UNEMPLOYED
-		pumsToEd[0] = 26;
+    	industryIndex = getIndustryIndex("UNEMPLOYED");
+		pumsToEd[0] = industryIndex;
     }
 
     // return the IncomeSize category index given the label.
