4b777fb7a66536d720be3efa352ab8328ab13d49
christi willison <willison@pbworld.com>
2004-12-22 23:58:09 +0000

    changed constructor to accept global.properties file.  It will be used to define the max zone number which is currently hard coded.
    git-svn-id: http://216.243.97.229:9090/svn/projects/trunk/tlumip@234 ee1c2863-5742-f242-9f82-d2f51c100d09

--- src/java/com/pb/despair/ct/CTModel.java
+++ src/java/com/pb/despair/ct/CTModel.java
@@ -22,15 +22,15 @@ public class CTModel extends ModelComponent {
     String outputPath;   //run the CTModel.
     boolean debug;      //in properties file
 
-	public CTModel(ResourceBundle rb){
-		setProperties(rb);    //creates a resource bundle as a class attribute called resourceBundle.
-        this.debug = (Boolean.valueOf(ResourceUtil.getProperty(rb, "debug"))).booleanValue();
+	public CTModel(ResourceBundle appRb, ResourceBundle globalRb){
+		setResourceBundles(appRb, globalRb);    //creates a resource bundle as a class attribute called appRb.
+        this.debug = (Boolean.valueOf(ResourceUtil.getProperty(appRb, "debug"))).booleanValue();
         logger.info("debug: " + debug);
-        this.inputPath = ResourceUtil.getProperty(rb, "ct.base.data");
+        this.inputPath = ResourceUtil.getProperty(appRb, "ct.base.data");
         logger.info("inputPath: " + inputPath);
-        this.outputPath = ResourceUtil.getProperty(rb, "ct.current.data");
+        this.outputPath = ResourceUtil.getProperty(appRb, "ct.current.data");
         logger.info("outputPath: " + outputPath);
-        this.randomSeed = Long.parseLong(ResourceUtil.getProperty(rb, "randomSeed"));
+        this.randomSeed = Long.parseLong(ResourceUtil.getProperty(appRb, "randomSeed"));
         logger.info("random seed: " + randomSeed);
     }
 	
@@ -40,12 +40,12 @@ public class CTModel extends ModelComponent {
         // This translates PI output (annual dollar flows at beta zone level) to
         // weekly tons by commodity class (SCTG01-SCTG43), and writes output in
         // binary format.
-        FreightDemand3 fd = new FreightDemand3(resourceBundle,debug,inputPath,outputPath,randomSeed);
+        FreightDemand3 fd = new FreightDemand3(appRb,debug,inputPath,outputPath,randomSeed);
 	    fd.run();  //writes WeeklyDemand.binary
 
         // Translates weekly demand from beta zones into discrete daily shipments in
         // alpha zone, and writes text file of shipments
-        DiscreteShipments2 ds = new DiscreteShipments2(resourceBundle,inputPath,randomSeed);
+        DiscreteShipments2 ds = new DiscreteShipments2(appRb,inputPath,randomSeed);
         ds.run(new File(outputPath + "WeeklyDemand.binary"));   // input from FreightDemand
         ds.writeShipments(new File(outputPath + "DailyShipments.csv"));   // output file
 
@@ -53,7 +53,7 @@ public class CTModel extends ModelComponent {
         // their itinerary, and writes to a CSV file in format required by TS
         // Next line prevents writing intrazonal (at alpha level) trips
         boolean collapseIntrazonalTrips = true;
-        TruckTours4 truckTours = new TruckTours4(resourceBundle,inputPath,randomSeed);
+        TruckTours4 truckTours = new TruckTours4(appRb,inputPath,randomSeed);
         truckTours.run(new File(outputPath + "DailyShipments.csv"));   // input from DiscreteShipments
         truckTours.writeTours(new File(outputPath + "TruckTrips.csv"), collapseIntrazonalTrips);  //output
 
@@ -61,7 +61,7 @@ public class CTModel extends ModelComponent {
 	}
 	
 	public static void main (String args[]){
-		CTModel ctModel = new CTModel(ResourceUtil.getResourceBundle("ct"));
+		CTModel ctModel = new CTModel(ResourceUtil.getResourceBundle("ct"), ResourceUtil.getResourceBundle("global"));
 		ctModel.startModel(1);	
 	}
 
