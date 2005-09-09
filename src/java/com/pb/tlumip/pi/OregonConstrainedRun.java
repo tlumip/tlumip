dd1eb9665733dac8e264dda93ee259c16e25a09b
john abraham <jea@hbaspecto.com>
2005-09-09 16:39:35 +0000

    To test out the constraints
    git-svn-id: http://216.243.97.229:9090/svn/projects/trunk/tlumip@651 ee1c2863-5742-f242-9f82-d2f51c100d09

---
+++ src/java/com/pb/tlumip/pi/OregonConstrainedRun.java
@@ -0,0 +1,15 @@
+package com.pb.tlumip.pi;
+
+import com.pb.models.pecas.PiConstrainedControl;
+
+public class OregonConstrainedRun {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		PiConstrainedControl.main(args);
+
+	}
+
+}
