d456802916bc7e982a032948ba5fc7920fb876d4
jim hicks <hicksji@pbworld.com>
2005-10-25 21:30:34 +0000

    adding tsHandlers.groovy
    git-svn-id: http://216.243.97.229:9090/svn/projects/trunk/tlumip@794 ee1c2863-5742-f242-9f82-d2f51c100d09

---
+++ config/ts/tsHandlers.groovy
@@ -0,0 +1,21 @@
+//Define TS handler classes - these are the rpc end points
+
+//Default port to listen for connections for this node
+listenPort = 6001
+
+//Define node URLs
+nodes[0].name = "isis"
+nodes[0].url = "tcp://localhost:6001"
+
+//Define handler classes, these are the rpc-end points on this node
+handlers[0].name = "networkHandler"
+handlers[0].className = "com.pb.tlumip.ts.NetworkHandler"
+handlers[0].node = "isis"
+
+handlers[1].name = "demandHandler"
+handlers[1].className = "com.pb.tlumip.ts.DemandHandler"
+handlers[1].node = "isis"
+
+handlers[2].name = "shortestPathTreeHandler"
+handlers[2].className = "com.pb.tlumip.ts.ShortestPathTreeHandler"
+handlers[2].node = "isis"
