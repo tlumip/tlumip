6a69bacf44016cd43d0a3ef6f23b71e26e86a5c6
jim hicks <hicksji@pbworld.com>
2006-01-09 16:01:43 +0000

    corrected random number tracking for stop frequency.  initial shell of code for stop location model
    git-svn-id: http://216.243.97.229:9090/svn/projects/trunk/tlumip@1119 ee1c2863-5742-f242-9f82-d2f51c100d09

---
+++ config/ts/tsHandlersTest.groovy
@@ -0,0 +1,28 @@
+//Define TS handler classes - these are the rpc end points
+
+//Default port to listen for connections for this node
+listenPort = 6001
+
+//Define node URLs
+nodes[0].name = "thinkPad"
+nodes[0].url = "tcp://localhost:6001"
+
+nodes[1].name = "isis"
+nodes[1].url = "tcp://192.168.1.214:6001"
+
+//Define handler classes, these are the rpc-end points on this node
+handlers[0].name = "networkHandler"
+handlers[0].className = "com.pb.tlumip.ts.NetworkHandler"
+handlers[0].node = "thinkPad"
+
+handlers[1].name = "demandHandler"
+handlers[1].className = "com.pb.tlumip.ts.DemandHandler"
+handlers[1].node = "thinkPad"
+
+handlers[2].name = "shortestPathTreeHandler"
+handlers[2].className = "com.pb.tlumip.ts.ShortestPathTreeHandler"
+handlers[2].node = "thinkPad"
+
+handlers[3].name = "aonFlowHandler"
+handlers[3].className = "com.pb.tlumip.ts.AonFlowHandler"
+handlers[3].node = "thinkPad"
