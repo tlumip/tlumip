e3702a9ea6fdcf3dce7e43f85af2ae48c9102d60
kgrommes <kgrommes@pbworld.com>
2007-02-15 16:58:29 +0000

    synced with Ohio orchestrator
    git-svn-id: http://216.243.97.229:9090/svn/projects/trunk/tlumip@2837 ee1c2863-5742-f242-9f82-d2f51c100d09

---
+++ src/java/com/pb/tlumip/pt/daf/TLUMIPMicroSimulationServer.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright  2005 PB Consult Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package com.pb.tlumip.pt.daf;
+
+import com.pb.models.pt.daf.MSServerTask;
+import com.pb.tlumip.pt.PTOccupation;
+
+/**
+ * TLUMIPMicroSimulationServer is a class that ...
+ *
+ * @author Kimberly Grommes
+ * @version 1.0, Feb 15, 2007
+ *          Created by IntelliJ IDEA.
+ */
+public class TLUMIPMicroSimulationServer extends MSServerTask {
+
+    public void onStart(){
+        super.onStart(PTOccupation.NONE);
+    }
+}
