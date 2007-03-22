a8509230250d1f972c68f7071393e47704673c5d
christi willison <willison@pbworld.com>
2007-03-22 17:33:30 +0000

    testGetAvailableAntTargets
    git-svn-id: http://216.243.97.229:9090/svn/projects/trunk/tlumip@3005 ee1c2863-5742-f242-9f82-d2f51c100d09

--- src/python/server/tests/TestScenarioRunningMethods.py
+++ src/python/server/tests/TestScenarioRunningMethods.py
@@ -11,19 +11,32 @@ def testGetAvailableMachines():
 def testGetAvailableAntTargets():
     """
     """
+    targetResult = [
+    ['runED', 'Runs the Economic Development Model for a single year.\nNo dependencies.', ['scenarioName', 'baseScenarioName', 'baseYear', 't']],
+    ['runEDCalib', 'Runs the Economic Development Model in t=0 for calibration purposes.\nNo dependencies.', ['scenarioName', 'baseScenarioName', 'baseYear']],
+    ['runALD', 'Runs the Land Development Model for a single year.\nDepends on ED.', ['scenarioName', 'baseScenarioName', 'baseYear', 't']],
+    ['runSPG1', 'Runs Synthetic Population Generation- part 1 for a single year.\nDepends on ED.', ['scenarioName', 'baseScenarioName', 'baseYear', 't']],
+    ['runPI', 'Runs the Production Allocation Model in monolithic mode for a single year.\nDepends on ED, ALD and SPG1.', ['scenarioName', 'baseScenarioName', 'baseYear', 't']],
+    ['runPIDAF', 'Runs the Production Allocation Model in distributed mode for a single year.\nDepends on ED, ALD and SPG1.', ['scenarioName', 'baseScenarioName', 'baseYear', 't']],
+    ['runSPG2', 'Runs the Synthetic Population Generation - part 2 for a single year.\nDepends on SPG1 and PI', ['scenarioName', 'baseScenarioName', 'baseYear', 't']],
+    ['runPTDAF', 'Runs the Person Transport Model for a single year.\nDepends on PI and SPG2.', ['scenarioName', 'baseScenarioName', 'baseYear', 't']],
+    ['runCT', 'Runs the Commodity Transport Model for a single year.\nDepends on PI.', ['scenarioName', 'baseScenarioName', 'baseYear', 't']],
+    ['runET', 'Runs the External Transport Model for a single year.\nDepends on PI', ['scenarioName', 'baseScenarioName', 'baseYear', 't']],
+    ['runTS', 'Runs the Transport Supply Model for a single year in monolithic mode.\nDepends on PT, ET and CT', ['scenarioName', 'baseScenarioName', 'baseYear', 't']],
+    ['runTSDAF', 'Runs the Transport Supply Model for a single year in distributed mode\nDepends on PT, ET and CT', ['scenarioName', 'baseScenarioName', 'baseYear', 't']],
+    ['runED20Years', 'Runs the Economic Development model for 20 years, starting in t=1', ['scenarioName', 'baseScenarioName', 'baseYear']],
+    ['run1YearSpatial', 'Runs ED, ALD and PIDAF for a single year', ['scenarioName', 'baseScenarioName', 'baseYear', 't']],
+    ['run1YearSpatialCalibration', 'Runs ED, ALD and PIDAF for t=0', ['scenarioName', 'baseScenarioName', 'baseYear']],
+    ['run10YearSpatial', 'Runs ED, ALD and PIDAF for 10 years starting in t=1', ['scenarioName', 'baseScenarioName', 'baseYear']],
+    ['run1YearTransport', 'Runs SPG2, PTDAF, ET, CT, and TSDAF for a single year\nDepends on ED, ALD and PI', ['scenarioName', 'baseScenarioName', 'baseYear', 't']],
+    ['run1YearCalibration', 'Runs ED, ALD, SPG1, PIDAF, SPG2, PTDAF, ET, CT, and TSDAF\nin t=0.', ['scenarioName', 'baseScenarioName', 'baseYear']],
+    ['run1Year', 'Runs ED, ALD, SPG1, PIDAF, SPG2, PTDAF, ET, CT, and TSDAF for a single year', ['scenarioName', 'baseScenarioName', 'baseYear', 't']],
+    ['run4Year', 'Runs ED, ALD, SPG1, PIDAF, SPG2, PTDAF, ET, CT, and TSDAF for 4 years, starting in t=1', ['scenarioName', 'baseScenarioName', 'baseYear']],
+    ['run16Year', 'Runs ED, ALD, SPG1, PIDAF, SPG2, PTDAF, ET, CT, and TSDAF for 16 years, starting in t=1', ['scenarioName', 'baseScenarioName', 'baseYear']],
+    ['run31Year', 'Runs ED, ALD, SPG1, PIDAF, SPG2, PTDAF, ET, CT, and TSDAF for 31 years, starting in t=1', ['scenarioName', 'baseScenarioName', 'baseYear']],
+    ]
     result = server.getAvailableAntTargets()
-    for r in result:
-        print r
-        for e in result[r]:
-            print "\t", e
-
-
-testGetAvailableAntTargets()
-
-def testGetAvailableAntTargets_Failure():
-    """
-    """
-    assert server.getAvailableAntTargets() == "unimplemented"
+    assert result == targetResult
 
 def testGetScenarioProperties():
     """
