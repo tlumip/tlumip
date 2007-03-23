ab2c5fbea8ecdbf0ea2d3aff94c68d9ef2270690
christi willison <willison@pbworld.com>
2007-03-23 15:04:49 +0000

    git-svn-id: http://216.243.97.229:9090/svn/projects/trunk/tlumip@3021 ee1c2863-5742-f242-9f82-d2f51c100d09

---
+++ src/python/server/TargetRules.py
@@ -0,0 +1,11 @@
+# TargetRules
+
+def default(scenario, baseScenario, baseYear, year):
+    print "default"
+
+def target1(scenario, baseScenario, baseYear, year):
+    print "target1"
+
+def target2(scenario, baseScenario, baseYear, year):
+    print "target2"
+
