86464aa02ce35fe7c72486c8ba2c4bc3ca853b51
kgrommes <kgrommes@pbworld.com>
2007-10-10 18:45:17 +0000

    adding detail (before was shell)
    git-svn-id: http://216.243.97.229:9090/svn/projects/trunk/tlumip@3767 ee1c2863-5742-f242-9f82-d2f51c100d09

--- src/java/com/pb/tlumip/et/ETModel.java
+++ src/java/com/pb/tlumip/et/ETModel.java
@@ -16,9 +16,15 @@
  */
 package com.pb.tlumip.et;
 
+import com.pb.common.datafile.CSVFileWriter;
+import com.pb.common.datafile.TableDataSet;
+import com.pb.common.util.ResourceUtil;
 import com.pb.models.reference.ModelComponent;
 import org.apache.log4j.Logger;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 /**
@@ -33,6 +39,8 @@ public class ETModel extends ModelComponent {
 
     Logger logger = Logger.getLogger(ETModel.class);
 
+    private ArrayList<ShipmentDetail> alTrucks;
+
 
     public ETModel(ResourceBundle appRb, ResourceBundle globalRb){
 		setResourceBundles(appRb, globalRb);    //creates a resource bundle as a class attribute called appRb.
@@ -41,6 +49,98 @@ public class ETModel extends ModelComponent {
 
 	public void startModel(int baseYear, int intTimeInterval){
         logger.info("Starting ET Model.");
+
+        alTrucks = new ArrayList<ShipmentDetail> ();
+        EEModel eeModel = new EEModel(appRb, globalRb);
+        eeModel.runModel(alTrucks);
+
+        EIModel eiModel = new EIModel(appRb, globalRb);
+        eiModel.runModel(alTrucks);
+
+        outputFile();
+
         logger.info("Finishing ET Model.");
     }
+
+    private void outputFile() {
+
+        int intTotalTrucks = 0;
+        TableDataSet tblOutput = new TableDataSet();
+
+        ArrayList<Integer> alOrigin = new ArrayList<Integer>();
+        ArrayList<Integer> alDestination = new ArrayList<Integer>();
+        ArrayList<String> alStartTime = new ArrayList<String>();
+        ArrayList<Integer> alTruckClass = new ArrayList<Integer>();
+
+        float fltRemainder = 0f;
+
+        //Iterate over all entries in alTrucks and write 1 row to the table for each trip.
+        for (ShipmentDetail objDetail : alTrucks) {
+            float fltValue = objDetail.getNumberOfTrucks();
+            fltRemainder += (fltValue - Math.floor(fltValue));
+
+            int intValue = (int) fltValue;
+            while (intValue > 0) {
+                intTotalTrucks++;
+                alOrigin.add(objDetail.getOrigination());
+                alDestination.add(objDetail.getDestination());
+                alStartTime.add(objDetail.getTimeOfDay());
+                alTruckClass.add(objDetail.getTruckClass());
+                intValue--;
+            }
+            float fltRemainderToDisperse = (float) Math.floor(fltRemainder);
+            fltRemainder -= fltRemainderToDisperse;
+
+            while (fltRemainderToDisperse > 0) {
+                intTotalTrucks++;
+                alOrigin.add(objDetail.getOrigination());
+                alDestination.add(objDetail.getDestination());
+                alStartTime.add(objDetail.getTimeOfDay());
+                alTruckClass.add(objDetail.getTruckClass());
+                fltRemainderToDisperse--;
+            }
+        }
+
+        if (fltRemainder > 0) {
+            ShipmentDetail objDetail = alTrucks.get(alTrucks.size()-1);
+            intTotalTrucks++;
+            alOrigin.add(objDetail.getOrigination());
+            alDestination.add(objDetail.getDestination());
+            alStartTime.add(objDetail.getTimeOfDay());
+            alTruckClass.add(objDetail.getTruckClass());
+        }
+
+        logger.info("ET Total trucks: " + intTotalTrucks);
+
+        int[] intsOrigin = new int[alOrigin.size()];
+        int[] intsDestination = new int[alOrigin.size()];
+        String[] strsStartTime = new String[alOrigin.size()];
+        int[] intsTruckClass = new int[alOrigin.size()];
+
+        for (int i = 0; i < alOrigin.size(); i++) {
+            intsOrigin[i] = alOrigin.get(i);
+            intsDestination[i] = alDestination.get(i);
+            strsStartTime[i] = alStartTime.get(i);
+            intsTruckClass[i] = alTruckClass.get(i);
+        }
+
+        tblOutput.appendColumn(intsOrigin, "Origin");
+        tblOutput.appendColumn(intsDestination, "Destination");
+        tblOutput.appendColumn(strsStartTime, "StartTime");
+        tblOutput.appendColumn(intsTruckClass, "TruckClass");
+
+        String strOutputPath = ResourceUtil.getProperty(appRb, "et.current.data");
+        String strFileName = ResourceUtil.getProperty(appRb, "ET.file.prefix") + "TruckTrips" + ".csv";
+
+        try{
+            CSVFileWriter cfwWriter = new CSVFileWriter();
+            cfwWriter.writeFile(tblOutput, new File(strOutputPath + strFileName));
+            cfwWriter.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
 }
