136192d8b63a38c5c4e0688e0fbef9f9499f2326
christi willison <willison@pbworld.com>
2007-10-15 23:14:22 +0000

    managed to delete from repository accidentally
    git-svn-id: http://216.243.97.229:9090/svn/projects/trunk/tlumip@3804 ee1c2863-5742-f242-9f82-d2f51c100d09

---
+++ src/java/com/pb/tlumip/ao/Entry.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright  2005 PB Consult Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package com.pb.tlumip.ao;
+
+/**
+ * This class defines valid entries for the command file
+ * which is used to start and stop daf applications
+ *
+ * @author Christi Willison
+ * @version Jun 7, 2004
+ */
+public class Entry {
+
+    public static String START_NODE = "StartNode";
+    public static String START_CLUSTER = "StartCluster";
+    public static String START_APPLICATION = "StartApplication";
+    public static String STOP_NODE = "StopNode";
+
+    public static String NODE0 = "node0";
+    public static String NODE1 = "node1";
+    public static String NODE2 = "node2";
+    public static String NODE3 = "node3";
+    public static String NODE4 = "node4";
+    public static String NODE5 = "node5";
+    public static String NODE6 = "node6";
+    public static String NODE7 = "node7";
+
+    public static String PIDAF = "pidaf";
+    public static String PTDAF = "ptdaf";
+}
---
+++ src/java/com/pb/tlumip/ao/Scenario.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright  2005 PB Consult Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package com.pb.tlumip.ao;
+
+/**
+ *  Contains the description for the TLUMIP
+ * scenario specified in AOProperties.xml
+ * 
+ * @author  Christi Willison
+ * @version Jan 7, 2004
+ * Created by IntelliJ IDEA.
+ */
+public class Scenario {
+    private String name;
+    private String start;
+    private String end;
+
+
+//------- Setter methods ------
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+
+
+//------- Getter methods -------
+    public String getName() {
+        return name;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public String toString(){
+            StringBuffer buf = new StringBuffer("Scenario: Name='"+name+"'\n");
+            buf.append("          Start="+start+"\n");
+            buf.append("          End="+end+"\n");
+            return buf.toString();
+        }
+
+}
