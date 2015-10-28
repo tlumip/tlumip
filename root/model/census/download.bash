47bae4e5b60d3fb93a73b1ddf146337557f8e7ee
Greg Macfarlane <gregmacfarlane@gmail.com>
2015-10-28 13:53:17 -0400

    Expand census file source handling.
    Added a bash script to download and expand all of the necessary PUMS files directly from the Census servers.
    
    
    Former-commit-id: 37f7e939ccc5836f38bc997ea6bdcc77e5448844

---
+++ root/model/census/download.bash
@@ -0,0 +1,10 @@
+
+for region in "por" "pnv" "pid" "pca" "hwa" "hor" "hnv" "hid" "hca"
+do
+  wget -O ss09$region.zip "http://www2.census.gov/programs-surveys/acs/data/pums/2009/5-Year/csv_${region}.zip"
+  unzip ss09$region.zip
+  rm *.pdf
+done
+
+rm *zip
+  
