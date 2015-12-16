7cd03cd0f64174bf085e0e46c534fbd3c978eaa2
Greg Macfarlane <macfarlaneg@pbworld.com>
2015-12-16 13:37:23 -0500

    Add pwa to downloaded pums files

--- root/model/census/download.bash
+++ root/model/census/download.bash
@@ -1,5 +1,5 @@
 
-for region in "por" "pnv" "pid" "pca" "hwa" "hor" "hnv" "hid" "hca"
+for region in "pwa" "por" "pnv" "pid" "pca" "hwa" "hor" "hnv" "hid" "hca"
 do
   wget -O ss09$region.zip "http://www2.census.gov/programs-surveys/acs/data/pums/2009/5-Year/csv_${region}.zip"
   unzip ss09$region.zip
