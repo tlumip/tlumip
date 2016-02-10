4515a522034ff5d665bd40e66c1d12395e61bd9a
Greg Macfarlane <gregmacfarlane@gmail.com>
2016-02-10 10:19:44 -0800

    Deleted old python report files
    Found a bunch of unused python code that hadn't changed in 8 years.

--- src/python/server/AOServerDocumentation.py
+++
@@ -1,5 +0,0 @@
-from TLUMIP_ApplicationOrchestratorServer import ApplicationOrchestratorServer
-import pydoc, webbrowser
-
-pydoc.writedoc(ApplicationOrchestratorServer)
-webbrowser.open_new("ApplicationOrchestratorServer.html")
--- src/python/server/tests/CSVBasicTest.py
+++
@@ -1,9 +0,0 @@
-import csv
-file("test.csv", 'w')
-writer = csv.writer(file("test.csv", "ab+"))
-writer.writerow(["a", "b", 1, 2, 3])
-writer.writerow(["a", "b", 1, 2, 4])
-writer.writerow(["a", "b", 1, 2, 5])
-writer = None
-reader = csv.reader(file("test.csv", "rb"))
-for row in reader: print row
