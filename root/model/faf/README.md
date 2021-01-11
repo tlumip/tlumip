700234508d780ffe7403ea9cc6ceb0d78ee635b4
Aditya Gore <aditya.gore@rsginc.com>
2021-01-11 12:07:06 -0500

    Created faf folder to host the FAF files and updated globalTemplate.properties file to point to FAF data source. Issue #88

---
+++ root/model/faf/README.md
@@ -0,0 +1,8 @@
+# FAF input files
+
+These files are needed from the Freight Analysis Framework (Bureau of Transportation Statistics) but are not included in the repository due to filesize:
+- FAF4.5.1.csv
+
+Users can download all of the necessary files by running `download.bash` in a bash shell.
+If the link is not active then user can access the latest FAF file here:
+https://www.bts.gov/faf/
---
+++ root/model/faf/download.bash
@@ -0,0 +1,10 @@
+set faf_name="FAF4.5.1_csv"
+echo "Downloading ${faf_name}.zip file"
+wget -O $faf_name.zip "https://www.bts.gov/sites/bts.dot.gov/files/legacy/AdditionalAttachmentFiles/${faf_name}.zip"
+if [ $? -ne 0]
+	then 
+		echo "${faf_name}.zip not found. Please visit https://www.bts.gov/faf/ to download the latest FAF file and update the globalTemplate.properties accordingly."
+	else 
+		unzip $faf_name.zip
+		rm *zip
+fi
